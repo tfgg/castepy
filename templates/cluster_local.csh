5b3d95af64c682ad7b7c2dff880b920991e91d7b
Tim Green <timothy.green@gmail.com>
2011-06-23 14:54:07 +0100

    Genericise cluster config for .sh files

---
+++ templates/cluster_local.csh
@@ -0,0 +1,12 @@
+#!/bin/tcsh
+
+# Kittel config
+
+module load intel/ict/4.0
+module load ofed/openmpi/intel/1.2.8
+
+# Ironman config
+
+#module load intel/ics-2011
+#module load ofed/openmpi/intel/1.4.3
+
--- templates/nmr/nmr.sh
+++ templates/nmr/nmr.sh
@@ -5,17 +5,11 @@
 #$ -V
 #$ -l qname=parallel.q
 
-module load intel/ics-2011
-module load ofed/openmpi/intel/1.4.3
-
+source /home/green/pylib/castepy/templates/cluster_local.csh
 source /opt/gridengine/default/common/settings.csh
 
 echo Job using the following number of cores: $NSLOTS
 echo Job starting on: `date`
-
-#echo $TMPDIR/machines
-#cat $TMPDIR/machines
-ls $TMPDIR
 
 mpirun -np $NSLOTS castep %(seedname)s
 
