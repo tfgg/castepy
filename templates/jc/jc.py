85ae3dc94ca1aff03b1d813775f7b613901911d4
Tim Green <timothy.green@gmail.com>
2012-02-27 12:54:49 +0000

    Fix MS calculations

--- templates/jc/jc.py
+++ templates/jc/jc.py
@@ -6,8 +6,6 @@ from castepy import castepy, pot, cell
 from castepy.calc import CastepCalc
 from castepy.util import calc_from_path
 
-pot_dir = "/home/green/scratch/ncp_pspot/"
-
 jc_path = os.path.join(settings.CASTEPY_ROOT, "templates/jc")
 
 merge_cell = cell.Cell(open(os.path.join(jc_path, "jc.cell")).read())
@@ -16,7 +14,7 @@ def make(source_dir, source_name, target_dir, target_name=None, jc_s=None, jc_i=
   calc = CastepCalc(source_dir, source_name)
   c = cell.Cell(calc.cell_file)
 
-  _, required_files= pot.add_potentials(pot_dir, None, c)
+  _, required_files= pot.add_potentials(settings.NCP_PSPOTS_DIR, None, c)
   pot.link_files(required_files, target_dir)
 
   c.other = []
--- templates/nmr/nmr.py
+++ templates/nmr/nmr.py
@@ -1,11 +1,10 @@
 import sys, os
 import shutil
 
-from castepy import castepy, pot, cell
+from castepy import castepy, cell
 from castepy.calc import CastepCalc
 from castepy.util import calc_from_path
 
-pot_dir = "/home/green/scratch/ncp_pspot/"
 nmr_path = "/home/green/pylib/castepy/templates/nmr"
 
 merge_cell = cell.Cell(open(os.path.join(nmr_path, "nmr.cell")).read())
@@ -23,7 +22,7 @@ def make(source_dir, source_name, target_dir, target_name=None):
   param_target = os.path.join(target_dir, "%s.param" % target_name)
   sh_target = os.path.join(target_dir, "%s.sh" % target_name)
 
-  sh_context = {'seedname': target_name, }
+  sh_context = {'seedname': target_name, 'CASTEPY_ROOT': settings.CASTEPY_ROOT}
   sh_source = open(os.path.join(nmr_path, "nmr.sh")).read()
   sh_target_file = open(sh_target, "w+")
 
